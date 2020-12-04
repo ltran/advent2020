ac7b5761380fb3369b00c4d749bb93579e11ce95
Louis Tran <louis.tran@omadahealth.com>
2020-12-04 15:41:46 -0800

    Day 2

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"strings"
 
 	"github.com/ltran/advent2020/day1"
+	"github.com/ltran/advent2020/day2"
 )
 
 func main() {
 	fmt.Println("day1-1:", day1.SumTwo(intstringToIntSlice(day1.INPUT), 2020))
 	fmt.Println("day1-2:", day1.SumThree(intstringToIntSlice(day1.INPUT), 2020))
+
+	fmt.Println("day2-1:", day2.CountValid(stringToStringSlice(day2.INPUT), false))
+	fmt.Println("day2-1:", day2.CountValid(stringToStringSlice(day2.INPUT), true))
 }
 
 func intstringToIntSlice(str string) []int {
@@ -20,4 +24,8 @@ func intstringToIntSlice(str string) []int {
 		entries[i], _ = strconv.Atoi(e)
 	}
 	return entries
+}
+
+func stringToStringSlice(str string) []string {
+	return strings.Split(str, "\n")
 }
